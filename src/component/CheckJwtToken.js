816e3c6055ebd296801c9ca7ab229f3c4b37237a
k5545332 <k5545332@gmail.com>
2021-10-06 09:31:56 +0800

    新增jwt期限驗證判斷

--- src/component/CheckJwtToken.js
+++ src/component/CheckJwtToken.js
@@ -2,10 +2,12 @@ import jwt_decode from "jwt-decode";
 
 const CheckJwtToken = ()=>{
   const Token = localStorage.getItem("LoginToken");
-  var decoded = jwt_decode(Token);
-  var expire = decoded.exp*1000;
-  if (expire <= Date.now()) {
-    localStorage.removeItem("LoginToken");
+  if (Token!=null) {
+    var decoded = jwt_decode(Token);
+    var expire = decoded.exp*1000;
+    if (expire <= Date.now()) {
+      localStorage.removeItem("LoginToken");
+    }
   }
 }
 
